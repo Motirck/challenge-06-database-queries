aa3b4311efad9283bc4cf55f2a0a1ce4ba46437c
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-07-25 16:29:46 -0300

    challenges in progress. Missing 1 test

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -11,20 +11,22 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
+  async findUserWithGamesById({ user_id, }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne({ where: { id: user_id}, relations: ["games"]});
+    return user!;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE first_name = ${first_name} AND last_name = ${last_name}`);
   }
 }
